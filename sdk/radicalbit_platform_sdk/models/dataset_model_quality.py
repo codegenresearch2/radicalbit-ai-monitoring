a8fc731948bda2796093f153b81663e3230efd02
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 17:12:11 +0000

    Reset to 0db1dfe365adb7a1973b6302359bc18c158bf720 and made changes for LLM Round 1

--- sdk/radicalbit_platform_sdk/models/dataset_model_quality.py
+++ sdk/radicalbit_platform_sdk/models/dataset_model_quality.py
@@ -2,24 +2,19 @@ from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
 from typing import Optional
 
-
-class ModelQuality(BaseModel):
-    pass
-
-
-class BinaryClassificationModelQuality(ModelQuality):
-    f1: Optional[float] = None
-    accuracy: Optional[float] = None
-    precision: Optional[float] = None
-    recall: Optional[float] = None
-    f_measure: Optional[float] = None
-    weighted_precision: Optional[float] = None
-    weighted_recall: Optional[float] = None
-    weighted_f_measure: Optional[float] = None
-    weighted_true_positive_rate: Optional[float] = None
-    weighted_false_positive_rate: Optional[float] = None
-    true_positive_rate: Optional[float] = None
-    false_positive_rate: Optional[float] = None
+class DatasetStats(BaseModel):
+    f1: float
+    accuracy: float
+    precision: float
+    recall: float
+    f_measure: float
+    weighted_precision: float
+    weighted_recall: float
+    weighted_f_measure: float
+    weighted_true_positive_rate: float
+    weighted_false_positive_rate: float
+    true_positive_rate: float
+    false_positive_rate: float
     true_positive_count: int
     false_positive_count: int
     true_negative_count: int
@@ -30,11 +25,3 @@ class BinaryClassificationModelQuality(ModelQuality):
     model_config = ConfigDict(
         populate_by_name=True, alias_generator=to_camel, protected_namespaces=()
     )
-
-
-class MultiClassModelQuality(ModelQuality):
-    pass
-
-
-class RegressionModelQuality(ModelQuality):
-    pass
