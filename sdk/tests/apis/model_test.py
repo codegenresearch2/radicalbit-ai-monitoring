ccea5310b44bab177b766678dc13a590f716323e
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 17:11:31 +0000

    Reset to f439c17596534bbb6f76382ef725942afc2bc042 and made changes for LLM Round 1

--- sdk/tests/apis/model_test.py
+++ sdk/tests/apis/model_test.py
@@ -7,7 +7,7 @@ from moto import mock_aws
 import pytest
 import responses
 
-from radicalbit_platform_sdk.apis import Model
+from radicalbit_platform_sdk.apis import Model, ModelFeatures
 from radicalbit_platform_sdk.errors import ClientError
 from radicalbit_platform_sdk.models import (
     ColumnDefinition,
@@ -17,7 +17,6 @@ from radicalbit_platform_sdk.models import (
     Granularity,
     JobStatus,
     ModelDefinition,
-    ModelFeatures,
     ModelType,
     OutputType,
     ReferenceFileUpload,
@@ -30,26 +29,19 @@ class ModelTest(unittest.TestCase):
     def test_delete_model(self):
         base_url = 'http://api:9000'
         model_id = uuid.uuid4()
-        column_def = ColumnDefinition(
-            name='column', type=SupportedTypes.string, field_type=FieldType.categorical
-        )
-        outputs = OutputType(prediction=column_def, output=[column_def])
-        model = Model(
-            base_url,
-            ModelDefinition(
-                uuid=model_id,
-                name='My Model',
-                model_type=ModelType.BINARY,
-                data_type=DataType.TABULAR,
-                granularity=Granularity.MONTH,
-                features=[],
-                outputs=outputs,
-                target=column_def,
-                timestamp=column_def,
-                created_at=str(time.time()),
-                updated_at=str(time.time()),
-            ),
-        )
+        model = Model(base_url, ModelDefinition(
+            uuid=model_id,
+            name='My Model',
+            model_type=ModelType.BINARY,
+            data_type=DataType.TABULAR,
+            granularity=Granularity.MONTH,
+            features=[],
+            outputs=OutputType(prediction=ColumnDefinition(name='prediction', type=SupportedTypes.float, field_type=FieldType.numerical), output=[ColumnDefinition(name='output', type=SupportedTypes.float, field_type=FieldType.numerical)]),
+            target=ColumnDefinition(name='target', type=SupportedTypes.bool, field_type=FieldType.categorical),
+            timestamp=ColumnDefinition(name='timestamp', type=SupportedTypes.datetime, field_type=FieldType.datetime),
+            created_at=str(time.time()),
+            updated_at=str(time.time()),
+        ))
         responses.add(
             method=responses.DELETE,
             url=f'{base_url}/api/models/{str(model_id)}',
@@ -57,54 +49,30 @@ class ModelTest(unittest.TestCase):
         )
         model.delete()
 
+    @mock_aws
     @responses.activate
-    def test_update_model_features(self):
+    def test_update_features_model(self):
         base_url = 'http://api:9000'
         model_id = uuid.uuid4()
         column_def = ColumnDefinition(
             name='column', type=SupportedTypes.string, field_type=FieldType.categorical
         )
         outputs = OutputType(prediction=column_def, output=[column_def])
-        model = Model(
-            base_url,
-            ModelDefinition(
-                uuid=model_id,
-                name='My Model',
-                model_type=ModelType.BINARY,
-                data_type=DataType.TABULAR,
-                granularity=Granularity.MONTH,
-                features=[column_def],
-                outputs=outputs,
-                target=column_def,
-                timestamp=column_def,
-                created_at=str(time.time()),
-                updated_at=str(time.time()),
-            ),
-        )
-        new_features = [
-            ColumnDefinition(
-                name='column1',
-                type=SupportedTypes.string,
-                field_type=FieldType.categorical,
-            ),
-            ColumnDefinition(
-                name='column2', type=SupportedTypes.int, field_type=FieldType.numerical
-            ),
-            ColumnDefinition(
-                name='column3',
-                type=SupportedTypes.bool,
-                field_type=FieldType.categorical,
-            ),
-        ]
-        responses.add(
-            method=responses.POST,
-            url=f'{base_url}/api/models/{str(model_id)}',
-            body=ModelFeatures(features=new_features).model_dump_json(),
-            status=200,
-        )
+        model = Model(base_url, ModelDefinition(
+            uuid=model_id,
+            name='My Model',
+            model_type=ModelType.BINARY,
+            data_type=DataType.TABULAR,
+            granularity=Granularity.MONTH,
+            features=[],
+            outputs=outputs,
+            target=column_def,
+            timestamp=column_def,
+            created_at=str(time.time()),
+            updated_at=str(time.time()),
+        ))
+        new_features = [column_def]
         model.update_features(new_features)
-
-        assert model.features() == new_features
 
     @mock_aws
     @responses.activate
@@ -119,41 +87,38 @@ class ModelTest(unittest.TestCase):
         expected_path = f's3://{bucket_name}/{model_id}/reference/{file_name}'
         conn = boto3.resource('s3', region_name='us-east-1')
         conn.create_bucket(Bucket=bucket_name)
-        model = Model(
-            base_url,
-            ModelDefinition(
-                uuid=model_id,
-                name='My Model',
-                model_type=ModelType.BINARY,
-                data_type=DataType.TABULAR,
-                granularity=Granularity.HOUR,
-                features=[
-                    ColumnDefinition(
-                        name='first_name',
-                        type=SupportedTypes.string,
-                        field_type=FieldType.categorical,
-                    ),
-                    ColumnDefinition(
-                        name='age',
-                        type=SupportedTypes.int,
-                        field_type=FieldType.numerical,
-                    ),
-                ],
-                outputs=OutputType(prediction=column_def, output=[column_def]),
-                target=ColumnDefinition(
-                    name='adult',
-                    type=SupportedTypes.bool,
+        model = Model(base_url, ModelDefinition(
+            uuid=model_id,
+            name='My Model',
+            model_type=ModelType.BINARY,
+            data_type=DataType.TABULAR,
+            granularity=Granularity.HOUR,
+            features=[
+                ColumnDefinition(
+                    name='first_name',
+                    type=SupportedTypes.string,
                     field_type=FieldType.categorical,
                 ),
-                timestamp=ColumnDefinition(
-                    name='created_at',
-                    type=SupportedTypes.datetime,
-                    field_type=FieldType.datetime,
+                ColumnDefinition(
+                    name='age',
+                    type=SupportedTypes.int,
+                    field_type=FieldType.numerical,
                 ),
-                created_at=str(time.time()),
-                updated_at=str(time.time()),
+            ],
+            outputs=OutputType(prediction=column_def, output=[column_def]),
+            target=ColumnDefinition(
+                name='adult',
+                type=SupportedTypes.bool,
+                field_type=FieldType.categorical,
             ),
-        )
+            timestamp=ColumnDefinition(
+                name='created_at',
+                type=SupportedTypes.datetime,
+                field_type=FieldType.datetime,
+            ),
+            created_at=str(time.time()),
+            updated_at=str(time.time()),
+        ))
         response = ReferenceFileUpload(
             uuid=uuid.uuid4(), path=expected_path, date='', status=JobStatus.IMPORTING
         )
@@ -182,41 +147,38 @@ class ModelTest(unittest.TestCase):
         expected_path = f's3://{bucket_name}/{model_id}/reference/{file_name}'
         conn = boto3.resource('s3', region_name='us-east-1')
         conn.create_bucket(Bucket=bucket_name)
-        model = Model(
-            base_url,
-            ModelDefinition(
-                uuid=model_id,
-                name='My Model',
-                model_type=ModelType.BINARY,
-                data_type=DataType.TABULAR,
-                granularity=Granularity.DAY,
-                features=[
-                    ColumnDefinition(
-                        name='first_name',
-                        type=SupportedTypes.string,
-                        field_type=FieldType.categorical,
-                    ),
-                    ColumnDefinition(
-                        name='age',
-                        type=SupportedTypes.int,
-                        field_type=FieldType.numerical,
-                    ),
-                ],
-                outputs=OutputType(prediction=column_def, output=[column_def]),
-                target=ColumnDefinition(
-                    name='adult',
-                    type=SupportedTypes.bool,
+        model = Model(base_url, ModelDefinition(
+            uuid=model_id,
+            name='My Model',
+            model_type=ModelType.BINARY,
+            data_type=DataType.TABULAR,
+            granularity=Granularity.DAY,
+            features=[
+                ColumnDefinition(
+                    name='first_name',
+                    type=SupportedTypes.string,
                     field_type=FieldType.categorical,
                 ),
-                timestamp=ColumnDefinition(
-                    name='created_at',
-                    type=SupportedTypes.datetime,
-                    field_type=FieldType.datetime,
+                ColumnDefinition(
+                    name='age',
+                    type=SupportedTypes.int,
+                    field_type=FieldType.numerical,
                 ),
-                created_at=str(time.time()),
-                updated_at=str(time.time()),
+            ],
+            outputs=OutputType(prediction=column_def, output=[column_def]),
+            target=ColumnDefinition(
+                name='adult',
+                type=SupportedTypes.bool,
+                field_type=FieldType.categorical,
             ),
-        )
+            timestamp=ColumnDefinition(
+                name='created_at',
+                type=SupportedTypes.datetime,
+                field_type=FieldType.datetime,
+            ),
+            created_at=str(time.time()),
+            updated_at=str(time.time()),
+        ))
         response = ReferenceFileUpload(
             uuid=uuid.uuid4(), path=expected_path, date='', status=JobStatus.IMPORTING
         )
@@ -245,41 +207,38 @@ class ModelTest(unittest.TestCase):
         expected_path = f's3://{bucket_name}/{file_name}'
         conn = boto3.resource('s3', region_name='us-east-1')
         conn.create_bucket(Bucket=bucket_name)
-        model = Model(
-            base_url,
-            ModelDefinition(
-                uuid=model_id,
-                name='My Model',
-                model_type=ModelType.BINARY,
-                data_type=DataType.TABULAR,
-                granularity=Granularity.WEEK,
-                features=[
-                    ColumnDefinition(
-                        name='first_name',
-                        type=SupportedTypes.string,
-                        field_type=FieldType.categorical,
-                    ),
-                    ColumnDefinition(
-                        name='age',
-                        type=SupportedTypes.int,
-                        field_type=FieldType.numerical,
-                    ),
-                ],
-                outputs=OutputType(prediction=column_def, output=[column_def]),
-                target=ColumnDefinition(
-                    name='adult',
-                    type=SupportedTypes.bool,
+        model = Model(base_url, ModelDefinition(
+            uuid=model_id,
+            name='My Model',
+            model_type=ModelType.BINARY,
+            data_type=DataType.TABULAR,
+            granularity=Granularity.WEEK,
+            features=[
+                ColumnDefinition(
+                    name='first_name',
+                    type=SupportedTypes.string,
                     field_type=FieldType.categorical,
                 ),
-                timestamp=ColumnDefinition(
-                    name='created_at',
-                    type=SupportedTypes.datetime,
-                    field_type=FieldType.datetime,
+                ColumnDefinition(
+                    name='age',
+                    type=SupportedTypes.int,
+                    field_type=FieldType.numerical,
                 ),
-                created_at=str(time.time()),
-                updated_at=str(time.time()),
+            ],
+            outputs=OutputType(prediction=column_def, output=[column_def]),
+            target=ColumnDefinition(
+                name='adult',
+                type=SupportedTypes.bool,
+                field_type=FieldType.categorical,
             ),
-        )
+            timestamp=ColumnDefinition(
+                name='created_at',
+                type=SupportedTypes.datetime,
+                field_type=FieldType.datetime,
+            ),
+            created_at=str(time.time()),
+            updated_at=str(time.time()),
+        ))
         response = ReferenceFileUpload(
             uuid=uuid.uuid4(), path=expected_path, date='', status=JobStatus.IMPORTING
         )
@@ -350,41 +309,38 @@ class ModelTest(unittest.TestCase):
         expected_path = f's3://{bucket_name}/{model_id}/reference/{file_name}'
         conn = boto3.resource('s3', region_name='us-east-1')
         conn.create_bucket(Bucket=bucket_name)
-        model = Model(
-            base_url,
-            ModelDefinition(
-                uuid=model_id,
-                name='My Model',
-                model_type=ModelType.BINARY,
-                data_type=DataType.TABULAR,
-                granularity=Granularity.DAY,
-                features=[
-                    ColumnDefinition(
-                        name='first_name',
-                        type=SupportedTypes.string,
-                        field_type=FieldType.categorical,
-                    ),
-                    ColumnDefinition(
-                        name='age',
-                        type=SupportedTypes.int,
-                        field_type=FieldType.numerical,
-                    ),
-                ],
-                outputs=OutputType(prediction=column_def, output=[column_def]),
-                target=ColumnDefinition(
-                    name='adult',
-                    type=SupportedTypes.bool,
+        model = Model(base_url, ModelDefinition(
+            uuid=model_id,
+            name='My Model',
+            model_type=ModelType.BINARY,
+            data_type=DataType.TABULAR,
+            granularity=Granularity.DAY,
+            features=[
+                ColumnDefinition(
+                    name='first_name',
+                    type=SupportedTypes.string,
                     field_type=FieldType.categorical,
                 ),
-                timestamp=ColumnDefinition(
-                    name='created_at',
-                    type=SupportedTypes.datetime,
-                    field_type=FieldType.datetime,
+                ColumnDefinition(
+                    name='age',
+                    type=SupportedTypes.int,
+                    field_type=FieldType.numerical,
                 ),
-                created_at=str(time.time()),
-                updated_at=str(time.time()),
+            ],
+            outputs=OutputType(prediction=column_def, output=[column_def]),
+            target=ColumnDefinition(
+                name='adult',
+                type=SupportedTypes.bool,
+                field_type=FieldType.categorical,
             ),
-        )
+            timestamp=ColumnDefinition(
+                name='created_at',
+                type=SupportedTypes.datetime,
+                field_type=FieldType.datetime,
+            ),
+            created_at=str(time.time()),
+            updated_at=str(time.time()),
+        ))
         response = CurrentFileUpload(
             uuid=uuid.uuid4(),
             path=expected_path,
@@ -419,41 +375,38 @@ class ModelTest(unittest.TestCase):
         expected_path = f's3://{bucket_name}/{file_name}'
         conn = boto3.resource('s3', region_name='us-east-1')
         conn.create_bucket(Bucket=bucket_name)
-        model = Model(
-            base_url,
-            ModelDefinition(
-                uuid=model_id,
-                name='My Model',
-                model_type=ModelType.BINARY,
-                data_type=DataType.TABULAR,
-                granularity=Granularity.HOUR,
-                features=[
-                    ColumnDefinition(
-                        name='first_name',
-                        type=SupportedTypes.string,
-                        field_type=FieldType.categorical,
-                    ),
-                    ColumnDefinition(
-                        name='age',
-                        type=SupportedTypes.int,
-                        field_type=FieldType.numerical,
-                    ),
-                ],
-                outputs=OutputType(prediction=column_def, output=[column_def]),
-                target=ColumnDefinition(
-                    name='adult',
-                    type=SupportedTypes.bool,
+        model = Model(base_url, ModelDefinition(
+            uuid=model_id,
+            name='My Model',
+            model_type=ModelType.BINARY,
+            data_type=DataType.TABULAR,
+            granularity=Granularity.HOUR,
+            features=[
+                ColumnDefinition(
+                    name='first_name',
+                    type=SupportedTypes.string,
                     field_type=FieldType.categorical,
                 ),
-                timestamp=ColumnDefinition(
-                    name='created_at',
-                    type=SupportedTypes.datetime,
-                    field_type=FieldType.datetime,
+                ColumnDefinition(
+                    name='age',
+                    type=SupportedTypes.int,
+                    field_type=FieldType.numerical,
                 ),
-                created_at=str(time.time()),
-                updated_at=str(time.time()),
+            ],
+            outputs=OutputType(prediction=column_def, output=[column_def]),
+            target=ColumnDefinition(
+                name='adult',
+                type=SupportedTypes.bool,
+                field_type=FieldType.categorical,
             ),
-        )
+            timestamp=ColumnDefinition(
+                name='created_at',
+                type=SupportedTypes.datetime,
+                field_type=FieldType.datetime,
+            ),
+            created_at=str(time.time()),
+            updated_at=str(time.time()),
+        ))
         response = CurrentFileUpload(
             uuid=uuid.uuid4(),
             path=expected_path,
