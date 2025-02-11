3a1e3c2910a0354668400a87938f196550f78e8c
vatsal_venkatkrishna <vatsal_venkatkrishna@gcp-us2-1>
2025-02-11 18:49:12 +0000

    Reset to 0db1dfe365adb7a1973b6302359bc18c158bf720 and made changes for LLM Round 1

--- sdk/radicalbit_platform_sdk/models/dataset_data_quality.py
+++ sdk/radicalbit_platform_sdk/models/dataset_data_quality.py
@@ -1,5 +1,4 @@
 from pydantic import BaseModel, ConfigDict
-from pydantic.alias_generators import to_camel
 from typing import List, Optional, Union
 
 class MedianMetrics(BaseModel):
@@ -7,20 +6,14 @@ class MedianMetrics(BaseModel):
     median: Optional[float] = None
     perc_75: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
-
 class MissingValue(BaseModel):
     count: int
     percentage: Optional[float] = None
-
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
 class ClassMedianMetrics(BaseModel):
     name: str
     mean: Optional[float] = None
     median_metrics: MedianMetrics
-
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
 class FeatureMetrics(BaseModel):
     feature_name: str
@@ -28,38 +21,28 @@ class FeatureMetrics(BaseModel):
     missing_value: MissingValue
     mean: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
-
 class NumericalFeatureMetrics(FeatureMetrics):
     type: str = 'numerical'
     std: Optional[float] = None
     min: Optional[float] = None
     max: Optional[float] = None
     class_median_metrics: List[ClassMedianMetrics]
-    histogram: Optional[Histogram] = None  # Added histogram attribute
-
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    histogram: Optional[Histogram] = None
 
 class CategoryFrequency(BaseModel):
     name: str
     count: int
     frequency: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
-
 class CategoricalFeatureMetrics(FeatureMetrics):
     type: str = 'categorical'
     category_frequency: List[CategoryFrequency]
     distinct_value: int
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
-
 class Histogram(BaseModel):
     buckets: List[float]
     reference_values: List[int]
     current_values: Optional[List[int]] = None
-
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
 class DataQuality(BaseModel):
     pass
@@ -68,8 +51,6 @@ class BinaryClassificationDataQuality(DataQuality):
     n_observations: int
     class_metrics: List[ClassMetrics]
     feature_metrics: List[Union[NumericalFeatureMetrics, CategoricalFeatureMetrics]]
-
-    model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True, alias_generator=to_camel)
 
 class MultiClassDataQuality(DataQuality):
     pass
@@ -80,8 +61,6 @@ class RegressionDataQuality(DataQuality):
 class DataQualityDTO(BaseModel):
     job_status: str
     data_quality: Optional[Union[BinaryClassificationDataQuality, MultiClassDataQuality, RegressionDataQuality]] = None
-
-    model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True, alias_generator=to_camel)
 
 # Removed the invalid comment that caused the SyntaxError.
 
