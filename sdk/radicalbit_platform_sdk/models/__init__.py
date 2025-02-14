394f3b583a6f0c35add015ed291c827f7a245584
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 12:13:46 +0000

    Reset to f439c17596534bbb6f76382ef725942afc2bc042 and made changes for LLM Round 1

--- sdk/radicalbit_platform_sdk/models/__init__.py
+++ sdk/radicalbit_platform_sdk/models/__init__.py
@@ -4,8 +4,6 @@ from .data_type import DataType
 from .dataset_data_quality import (
     ClassificationDataQuality,
     CategoricalFeatureMetrics,
-    CategoryFrequency,
-    ClassMedianMetrics,
     ClassMetrics,
     DataQuality,
     FeatureMetrics,
@@ -14,12 +12,7 @@ from .dataset_data_quality import (
     NumericalFeatureMetrics,
     RegressionDataQuality,
 )
-from .dataset_drift import (
-    Drift,
-    DriftAlgorithm,
-    FeatureDrift,
-    FeatureDriftCalculation,
-)
+from .dataset_drift import Drift, DriftAlgorithm, FeatureDrift, FeatureDriftCalculation
 from .dataset_model_quality import (
     BinaryClassificationModelQuality,
     CurrentBinaryClassificationModelQuality,
@@ -30,56 +23,92 @@ from .dataset_model_quality import (
     RegressionModelQuality,
 )
 from .dataset_stats import DatasetStats
-from .field_type import FieldType
 from .file_upload_result import CurrentFileUpload, FileReference, ReferenceFileUpload
 from .job_status import JobStatus
-from .model_definition import (
-    CreateModel,
-    Granularity,
-    ModelDefinition,
-    ModelFeatures,
-    OutputType,
-)
+from .model_definition import CreateModel, Granularity, ModelDefinition, OutputType
 from .model_type import ModelType
 from .supported_types import SupportedTypes
 
-__all__ = [
-    'OutputType',
-    'Granularity',
-    'CreateModel',
-    'ModelDefinition',
-    'ModelFeatures',
-    'ColumnDefinition',
-    'JobStatus',
-    'DataType',
-    'ModelType',
-    'DatasetStats',
-    'ModelQuality',
-    'BinaryClassificationModelQuality',
-    'CurrentBinaryClassificationModelQuality',
-    'CurrentMultiClassificationModelQuality',
-    'MultiClassificationModelQuality',
-    'RegressionModelQuality',
-    'CurrentRegressionModelQuality',
-    'DataQuality',
-    'ClassificationDataQuality',
-    'RegressionDataQuality',
-    'ClassMetrics',
-    'MedianMetrics',
-    'MissingValue',
-    'ClassMedianMetrics',
-    'FeatureMetrics',
-    'NumericalFeatureMetrics',
-    'CategoryFrequency',
-    'CategoricalFeatureMetrics',
-    'DriftAlgorithm',
-    'FeatureDriftCalculation',
-    'FeatureDrift',
-    'Drift',
-    'ReferenceFileUpload',
-    'CurrentFileUpload',
-    'FileReference',
-    'AwsCredentials',
-    'SupportedTypes',
-    'FieldType',
-]
+class DynamicModelDefinition(ModelDefinition):
+    def add_feature(self, feature: ColumnDefinition):
+        self.features.append(feature)
+
+    def add_output(self, output: ColumnDefinition):
+        self.outputs.output.append(output)
+
+    def add_prediction_proba(self, prediction_proba: ColumnDefinition):
+        self.outputs.prediction_proba = prediction_proba
+
+    def add_target(self, target: ColumnDefinition):
+        self.target = target
+
+    def add_timestamp(self, timestamp: ColumnDefinition):
+        self.timestamp = timestamp
+
+# Example usage
+model = DynamicModelDefinition(
+    uuid="your-uuid",
+    name="your-model-name",
+    description="your-model-description",
+    model_type=ModelType.REGRESSION,
+    data_type=DataType.TABULAR,
+    granularity=Granularity.DAY,
+    features=[],
+    outputs=OutputType(prediction=ColumnDefinition(name="prediction", type=SupportedTypes.float, field_type=FieldType.numerical), output=[]),
+    created_at="2022-01-01",
+    updated_at="2022-01-02"
+)
+
+model.add_feature(ColumnDefinition(name="feature1", type=SupportedTypes.int, field_type=FieldType.numerical))
+model.add_output(ColumnDefinition(name="output1", type=SupportedTypes.string, field_type=FieldType.categorical))
+model.add_prediction_proba(ColumnDefinition(name="prediction_proba", type=SupportedTypes.float, field_type=FieldType.numerical))
+model.add_target(ColumnDefinition(name="target", type=SupportedTypes.float, field_type=FieldType.numerical))
+model.add_timestamp(ColumnDefinition(name="timestamp", type=SupportedTypes.datetime, field_type=FieldType.datetime))
+
+# Example API response for testing
+response = {
+    "uuid": "your-uuid",
+    "name": "your-model-name",
+    "description": "your-model-description",
+    "model_type": "REGRESSION",
+    "data_type": "TABULAR",
+    "granularity": "DAY",
+    "features": [
+        {
+            "name": "feature1",
+            "type": "int",
+            "field_type": "numerical"
+        }
+    ],
+    "outputs": {
+        "prediction": {
+            "name": "prediction",
+            "type": "float",
+            "field_type": "numerical"
+        },
+        "prediction_proba": {
+            "name": "prediction_proba",
+            "type": "float",
+            "field_type": "numerical"
+        },
+        "output": [
+            {
+                "name": "output1",
+                "type": "string",
+                "field_type": "categorical"
+            }
+        ]
+    },
+    "target": {
+        "name": "target",
+        "type": "float",
+        "field_type": "numerical"
+    },
+    "timestamp": {
+        "name": "timestamp",
+        "type": "datetime",
+        "field_type": "datetime"
+    },
+    "created_at": "2022-01-01",
+    "updated_at": "2022-01-02"
+}
