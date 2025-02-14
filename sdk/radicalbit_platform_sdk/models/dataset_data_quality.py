d4b92cd18f45bb70f306a7d06a4bc649fe797d64
vatsal_venkatkrishna <vatsal_venkatkrishna@gcpl4-eu-0>
2025-02-14 11:33:15 +0000

    Reset to 0db1dfe365adb7a1973b6302359bc18c158bf720 and made changes for LLM Round 1

--- sdk/radicalbit_platform_sdk/models/dataset_data_quality.py
+++ sdk/radicalbit_platform_sdk/models/dataset_data_quality.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, ConfigDict
 from pydantic.alias_generators import to_camel
-from typing import List, Optional, Union
+from typing import List, Optional
 
 
 class ClassMetrics(BaseModel):
@@ -8,7 +8,7 @@ class ClassMetrics(BaseModel):
     count: int
     percentage: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
 
 
 class MedianMetrics(BaseModel):
@@ -16,14 +16,14 @@ class MedianMetrics(BaseModel):
     median: Optional[float] = None
     perc_75: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel, protected_namespaces=())
 
 
 class MissingValue(BaseModel):
     count: int
     percentage: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel, protected_namespaces=())
 
 
 class ClassMedianMetrics(BaseModel):
@@ -31,7 +31,7 @@ class ClassMedianMetrics(BaseModel):
     mean: Optional[float] = None
     median_metrics: MedianMetrics
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel, protected_namespaces=())
 
 
 class FeatureMetrics(BaseModel):
@@ -39,15 +39,7 @@ class FeatureMetrics(BaseModel):
     type: str
     missing_value: MissingValue
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
-
-
-class Histogram(BaseModel):
-    buckets: List[float]
-    reference_values: List[int]
-    current_values: Optional[List[int]] = None
-
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel, protected_namespaces=())
 
 
 class NumericalFeatureMetrics(FeatureMetrics):
@@ -58,9 +50,8 @@ class NumericalFeatureMetrics(FeatureMetrics):
     max: Optional[float] = None
     median_metrics: MedianMetrics
     class_median_metrics: List[ClassMedianMetrics]
-    histogram: Histogram
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel, protected_namespaces=())
 
 
 class CategoryFrequency(BaseModel):
@@ -68,7 +59,7 @@ class CategoryFrequency(BaseModel):
     count: int
     frequency: Optional[float] = None
 
-    model_config = ConfigDict(populate_by_name=True)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel, protected_namespaces=())
 
 
 class CategoricalFeatureMetrics(FeatureMetrics):
@@ -76,7 +67,7 @@ class CategoricalFeatureMetrics(FeatureMetrics):
     category_frequency: List[CategoryFrequency]
     distinct_value: int
 
-    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel)
+    model_config = ConfigDict(populate_by_name=True, alias_generator=to_camel, protected_namespaces=())
 
 
 class DataQuality(BaseModel):
@@ -86,13 +77,9 @@ class DataQuality(BaseModel):
 class BinaryClassificationDataQuality(DataQuality):
     n_observations: int
     class_metrics: List[ClassMetrics]
-    feature_metrics: List[Union[NumericalFeatureMetrics, CategoricalFeatureMetrics]]
+    feature_metrics: List[FeatureMetrics]
 
-    model_config = ConfigDict(
-        arbitrary_types_allowed=True,
-        populate_by_name=True,
-        alias_generator=to_camel,
-    )
+    model_config = ConfigDict(arbitrary_types_allowed=True, populate_by_name=True, alias_generator=to_camel, protected_namespaces=())
 
 
 class MultiClassDataQuality(DataQuality):
